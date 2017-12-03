365ccb6ea8d6fca2d4f7a8be56b402a90f706856
David Phan <dphan2@student.unimelb.edu.au>
2017-12-03 12:12:15 +1100

    objects

--- exampleCopy/jsfile.js
+++ exampleCopy/jsfile.js
@@ -1,35 +1,24 @@
-document.getElementById('example').innerHTML = 'Go javascript';
+var house = {
+  address: "13 Evergreen Terrace",
+  size: 200,
+  rooms: 4,
+  style: "Greek palace",
+  openDoor: function() {
 
-//create variables a and b
-var a, b;
-a = 'hello';
-b = a + 'world';
+    console.log('Door opened');
 
-//change div html to hello world
+  }
 
-document.getElementById('example').innerHTML = b;
+};
 
-//creation of a function
-//function literals consist of function followed by fname followed by invocation
-//expression.
-function print(string){
 
-console.log(string);
+print(house.openDoor());
 
+
+
+function print(a){
+
+console.log(a);
 
 
 }
-var pString = "Basic print function";
-print(pString);
-
-
-
-
-var multiply = function(a, b) {
-
-  return a * b;
-
-}
-
-var product = multiply(3, 4);
-print(product);
